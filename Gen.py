92e59918dc0c6c0224c486932f2440b0e68cdc86
Iane501 <166268003+Iane501@users.noreply.github.com>
2024-04-29 17:41:52 -0600

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -1,84 +1,48 @@
-import random
-password_lenght = int(input("¿Que tan larga quieres que sea tu contraseña?"))
-Caracteres = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "#",
-    "!",
-    "+",
-    "-",
-    "/",
-    "*",
-    "?",
-    "@",
-    "$",
-    "&"
-    ]
-Password = []
+import discord
+from discord.ext import commands
 
-for i in range(password_lenght):
-    caracter_1 = random.randint(0, 70)
-    Password.append(Caracteres[caracter_1])
+intents = discord.Intents.default()
+intents.message_content = True
 
-contrasena = "".join(Password)
-print("Tu contraseña es:", contrasena)
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} no es cool...')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Si, el bot es cool!')
+
+@bot.command()
+async def guardar_y_enviar(ctx,*,texto):
+    canal_id="1234357971455311893"
+
+    canal_destino= bot.get_channel(int(canal_id))
+
+    if canal_destino is None:
+        await ctx.send("El canal no existe")
+        return
+    await canal_destino.send(texto)
+    await ctx.send("¡Mensaje enviado!")
+
+bot.run("MTIyOTU5ODE3Mzg1MzM4NDcyNQ.GYOpzF.tneK8YBEqXOERTT-B5snaTLzaRj5_lJB1l9SFk")
